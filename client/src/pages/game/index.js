b7e3397fd2de0d96d6a7e2d64f7f4a4ecc41589b
Trevor Blades <tdblades@gmail.com>
2018-05-29 09:55:40 -0700

    Add some more game page stuff

--- client/src/pages/game/index.js
+++ client/src/pages/game/index.js
@@ -1,13 +1,19 @@
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import Typography from '@material-ui/core/Typography';
 import map from 'lodash/map';
+import styled from 'react-emotion';
 import {connect} from 'react-redux';
 import NotFound from '../not-found';
+import theme from '../../theme';
 import {getFullName} from '../../util/skater';
 import {load as loadGame} from '../../actions/game';
+
+const Container = styled.div({
+  padding: theme.spacing.unit * 3
+});
 
 class Game extends Component {
   static propTypes = {
@@ -29,15 +35,15 @@ class Game extends Component {
       return <NotFound />;
     }
 
+    const title = map(this.props.game.skaters, getFullName).join(' vs. ');
     return (
-      <Fragment>
+      <Container>
         <Helmet>
-          <title>
-            {map(this.props.game.skaters, getFullName).join(' vs. ')}
-          </title>
+          <title>{title}</title>
         </Helmet>
         <Typography variant="title">{this.props.game.event.name}</Typography>
-      </Fragment>
+        <Typography variant="subheading">{title}</Typography>
+      </Container>
     );
   }
 }
