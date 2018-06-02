41d6b808af60fa0780611954d06e099031c12785
Trevor Blades <tdblades@gmail.com>
2018-06-02 12:51:53 -0700

    LInk the skater names at the top of a game

--- client/src/pages/game/header.js
+++ client/src/pages/game/header.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import React, {Component, Fragment} from 'react';
 import Typography from '@material-ui/core/Typography';
 import styled from 'react-emotion';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {getFullName} from '../../util/skater';
 import {getTitle} from '../../selectors/game';
@@ -37,7 +38,9 @@ class Header extends Component {
           </Typography>
           <Names>
             {this.props.game.skaters.map(skater => (
-              <Name key={skater.id}>{getFullName(skater)}</Name>
+              <Name key={skater.id}>
+                <Link to={`/skaters/${skater.id}`}>{getFullName(skater)}</Link>
+              </Name>
             ))}
           </Names>
         </div>
