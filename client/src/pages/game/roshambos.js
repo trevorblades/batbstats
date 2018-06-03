212625b1ea820b2b34a5f83f5ac3f64a29830ded
Trevor Blades <tdblades@gmail.com>
2018-06-02 23:13:02 -0700

    Figure out winners better

--- client/src/pages/game/roshambos.js
+++ client/src/pages/game/roshambos.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import Typography from '@material-ui/core/Typography';
+import find from 'lodash/find';
 import styled, {css} from 'react-emotion';
 import {connect} from 'react-redux';
 import {size} from 'polished';
@@ -20,8 +22,12 @@ const Container = styled.div({
 
 const Column = styled.div({
   width: '50%',
-  ':first-child': {
-    textAlign: 'right'
+  textAlign: 'right',
+  ':last-child': {
+    textAlign: 'left',
+    svg: {
+      transform: 'scaleX(-1)'
+    }
   }
 });
 
@@ -35,33 +41,45 @@ const icons = {
 
 class Roshambos extends Component {
   static propTypes = {
+    game: PropTypes.object.isRequired,
     rounds: PropTypes.array.isRequired,
     winner: PropTypes.number.isRequired
   };
 
+  renderWinner() {}
+
   render() {
-    return this.props.rounds.map((round, index, array) => (
-      <Container key={index.toString()}>
-        {round.map(roshambo => {
-          const Icon = icons[roshambo.move];
-          const didLose =
-            index === array.length - 1 &&
-            roshambo.skater_id !== this.props.winner;
-          return (
-            <Column key={roshambo.id}>
-              <Icon
-                className={iconClassName}
-                fill={didLose ? theme.palette.grey[100] : null}
-              />
-            </Column>
-          );
-        })}
-      </Container>
-    ));
+    const winner = find(this.props.game.skaters, ['id', this.props.winner]);
+    return (
+      <Fragment>
+        {this.props.rounds.map((round, index, array) => (
+          <Container key={index.toString()}>
+            {round.map(roshambo => {
+              const Icon = icons[roshambo.move];
+              const didLose =
+                index === array.length - 1 &&
+                roshambo.skater_id !== this.props.winner;
+              return (
+                <Column key={roshambo.id}>
+                  <Icon
+                    className={iconClassName}
+                    fill={didLose ? theme.palette.grey[100] : null}
+                  />
+                </Column>
+              );
+            })}
+          </Container>
+        ))}
+        <Typography align="center" variant="button">
+          {winner.first_name} goes first
+        </Typography>
+      </Fragment>
+    );
   }
 }
 
 const mapStateToProps = state => ({
+  game: state.game.properties,
   rounds: getRoshamboRounds(state),
   winner: getRoshamboWinner(state)
 });
--- client/src/selectors/game.js
+++ client/src/selectors/game.js
@@ -1,25 +1,31 @@
 import eq from 'lodash/eq';
 import groupBy from 'lodash/groupBy';
 import map from 'lodash/map';
+import sortBy from 'lodash/sortBy';
 import values from 'lodash/values';
 import {createSelector} from 'reselect';
 import {ROSHAMBO_COUNTERS} from '../../../api/common';
 import {getFullName} from '../util/skater';
 
 const getGame = state => state.game.properties;
-export const getRoshamboRounds = createSelector(getGame, game =>
-  values(groupBy(game.roshambos, 'round'))
-);
+export const getRoshamboRounds = createSelector(getGame, game => {
+  const rounds = values(groupBy(game.roshambos, 'round'));
+  const skaterIds = map(game.skaters, 'id');
+  return rounds.map(round =>
+    sortBy(round, roshambo => skaterIds.indexOf(roshambo.skater_id))
+  );
+});
 
 export const getRoshamboWinner = createSelector(getRoshamboRounds, rounds => {
   for (let i = 0; i < rounds.length; i++) {
     const round = rounds[i];
     const moves = map(round, 'move');
-    if (eq(moves)) {
+    if (eq(...moves)) {
       continue;
     }
 
-    return round[ROSHAMBO_COUNTERS[moves[0]] === moves[1] ? 1 : 0].skater_id;
+    const winnerIndex = Number(ROSHAMBO_COUNTERS[moves[0]] === moves[1]);
+    return round[winnerIndex].skater_id;
   }
 });
 
