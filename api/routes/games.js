b7cd83641b18d8410478f636393d7a5db0512f84
Trevor Blades <tdblades@gmail.com>
2018-07-23 18:36:04 -0700

    Implement paging through games

--- api/routes/games.js
+++ api/routes/games.js
@@ -2,7 +2,14 @@ import express from 'express';
 import {Skater, Game, Event, Roshambo, Attempt, Trick} from '../models';
 
 const router = express.Router();
+
+const pageSize = 25;
 router.get('/', async (req, res) => {
+  let page = parseInt(req.query.page);
+  if (isNaN(page)) {
+    page = 1;
+  }
+
   const games = await Game.findAll({
     include: [
       Event,
@@ -13,10 +20,15 @@ router.get('/', async (req, res) => {
         include: Trick
       }
     ],
-    order: [[Attempt, 'id']]
+    order: [[Attempt, 'id']],
+    limit: pageSize,
+    offset: pageSize * (page - 1)
   });
 
-  res.send(games);
+  res.send({
+    games,
+    next_page: games.length < pageSize ? null : page + 1
+  });
 });
 
 export default router;
--- client/src/reducers/games.js
+++ client/src/reducers/games.js
@@ -3,8 +3,8 @@ import {loop, Cmd} from 'redux-loop';
 import {handleActions} from 'redux-actions';
 import {load, success, failure} from '../actions/games';
 
-async function fetchData() {
-  const response = await api.get('/games');
+async function fetchData(page = 1) {
+  const response = await api.get(`/games?page=${page}`);
   if (response.err) {
     throw response.body;
   }
@@ -35,11 +35,28 @@ export default handleActions(
       loading: false,
       error: payload
     }),
-    [success]: (state, {payload}) => ({
-      ...state,
-      loading: false,
-      data: payload
-    })
+    [success]: (state, {payload}) => {
+      const nextState = {
+        ...state,
+        data: [...state.data, ...payload.games]
+      };
+
+      if (payload.next_page) {
+        return loop(
+          nextState,
+          Cmd.run(fetchData, {
+            successActionCreator: success,
+            failActionCreator: failure,
+            args: [payload.next_page]
+          })
+        );
+      }
+
+      return {
+        ...nextState,
+        loading: false
+      };
+    }
   },
   defaultState
 );
