699625aa078cdd85d333e7edf8be8a20b7c930bf
Trevor Blades <tdblades@gmail.com>
2018-07-24 01:51:00 -0700

    Spruce up the not found page

--- client/src/pages/not-found.js
+++ client/src/pages/not-found.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import styled from 'react-emotion';
 
-const NotFound = () => <Typography variant="headline">Not found</Typography>;
+const Container = styled.div({
+  margin: 'auto',
+  textAlign: 'center'
+});
+
+const NotFound = () => (
+  <Container>
+    <Typography gutterBottom variant="display1">
+      Page not found
+    </Typography>
+    <Typography variant="subheading">That&apos;s a bummer 😓</Typography>
+  </Container>
+);
 
 export default NotFound;
