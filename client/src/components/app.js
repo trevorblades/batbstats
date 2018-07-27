a101a30737de79f3fb76eb2239edc039aea02dd9
Trevor Blades <tdblades@gmail.com>
2018-07-26 18:56:13 -0700

    Provide the app with router props

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -5,8 +5,10 @@ import PropTypes from 'prop-types';
 import React, {Component, Fragment} from 'react';
 import ReactGA from 'react-ga';
 import Sidebar from './sidebar';
+import compose from 'recompose/compose';
 import styled from 'react-emotion';
 import {hot} from 'react-hot-loader';
+import {withRouter} from 'react-router-dom';
 
 const Container = styled.div({
   display: 'flex',
@@ -42,4 +44,4 @@ class App extends Component {
   }
 }
 
-export default hot(module)(App);
+export default compose(hot(module), withRouter)(App);
