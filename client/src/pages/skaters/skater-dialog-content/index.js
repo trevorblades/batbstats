713452384299ea5a313b7230086d321a0121a1be
Trevor Blades <tdblades@gmail.com>
2018-07-31 01:40:19 -0700

    Split skater dialog into two columns

--- client/src/pages/skaters/skater-dialog-content/index.js
+++ client/src/pages/skaters/skater-dialog-content/index.js
@@ -3,13 +3,22 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
+import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 import React, {Component, Fragment} from 'react';
 import SkaterForm from './skater-form';
 import differenceInYears from 'date-fns/differenceInYears';
+import theme from '@trevorblades/mui-theme';
+import withProps from 'recompose/withProps';
 import {connect} from 'react-redux';
 
+const GridItem = withProps({
+  item: true,
+  xs: true
+})(Grid);
+
 const UNKNOWN = 'unknown';
+
 class SkaterDialogContent extends Component {
   static propTypes = {
     skater: PropTypes.object.isRequired,
@@ -41,21 +50,27 @@ class SkaterDialogContent extends Component {
       <Fragment>
         <DialogTitle>{this.props.skater.full_name}</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            Record: {this.props.skater.wins}-{this.props.skater.losses}
-          </DialogContentText>
-          <DialogContentText>
-            Stance: {this.props.skater.stance || UNKNOWN}
-          </DialogContentText>
-          <DialogContentText>
-            Age:{' '}
-            {this.props.skater.birth_date
-              ? differenceInYears(Date.now(), this.props.skater.birth_date)
-              : UNKNOWN}
-          </DialogContentText>
-          <DialogContentText>
-            Hometown: {this.props.skater.hometown || UNKNOWN}
-          </DialogContentText>
+          <Grid container spacing={theme.spacing.unit * 2}>
+            <GridItem>
+              <DialogContentText>
+                Record: {this.props.skater.wins}-{this.props.skater.losses}
+              </DialogContentText>
+              <DialogContentText>
+                Hometown: {this.props.skater.hometown || UNKNOWN}
+              </DialogContentText>
+            </GridItem>
+            <GridItem>
+              <DialogContentText>
+                Stance: {this.props.skater.stance || UNKNOWN}
+              </DialogContentText>
+              <DialogContentText>
+                Age:{' '}
+                {this.props.skater.birth_date
+                  ? differenceInYears(Date.now(), this.props.skater.birth_date)
+                  : UNKNOWN}
+              </DialogContentText>
+            </GridItem>
+          </Grid>
         </DialogContent>
         {this.props.user && (
           <DialogActions>
