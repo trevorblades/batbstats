7f0cb49b1a9d0ca75afd8a1b2366db2785083681
Trevor Blades <tdblades@gmail.com>
2018-08-16 00:47:49 -0700

    Add a game part of the dialog

--- client/src/pages/skaters/skater-dialog-content/index.js
+++ client/src/pages/skaters/skater-dialog-content/index.js
@@ -1,20 +1,24 @@
+import ButtonBase from '@material-ui/core/ButtonBase';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import FormDialogContent from '../../../components/form-dialog-content';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import SkaterForm from './skater-form';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
 import differenceInYears from 'date-fns/differenceInYears';
 import theme from '@trevorblades/mui-theme';
 import styled from 'react-emotion';
 import withProps from 'recompose/withProps';
+import {size} from 'polished';
 
 const StyledTable = styled(Table)({
   marginTop: theme.spacing.unit
@@ -25,65 +29,108 @@ const GridItem = withProps({
   xs: true
 })(Grid);
 
+const StyledChevronLeftIcon = styled(ChevronLeftIcon)(
+  size(theme.typography.title.lineHeight),
+  {marginLeft: theme.spacing.unit * -1}
+);
+
 const UNKNOWN = 'unknown';
 const NOW = Date.now();
 
-const SkaterDialogContent = props => (
-  <FormDialogContent data={props.skater} formComponent={SkaterForm}>
-    <DialogTitle>{props.skater.full_name}</DialogTitle>
-    <DialogContent>
-      <Grid container spacing={theme.spacing.unit * 2}>
-        <GridItem>
-          <DialogContentText>
-            Record: {props.skater.wins}-{props.skater.losses}
-          </DialogContentText>
-          <DialogContentText>
-            Hometown: {props.skater.hometown || UNKNOWN}
-          </DialogContentText>
-        </GridItem>
-        <GridItem>
-          <DialogContentText>
-            Stance: {props.skater.stance || UNKNOWN}
-          </DialogContentText>
-          {props.skater.birth_date && (
-            <DialogContentText>
-              Age: {differenceInYears(NOW, props.skater.birth_date)}
-            </DialogContentText>
-          )}
-        </GridItem>
-      </Grid>
-      <StyledTable padding="none">
-        <TableHead>
-          <TableRow>
-            <TableCell>Opponent</TableCell>
-            <TableCell>Event</TableCell>
-            <TableCell>Round</TableCell>
-            <TableCell>Result</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props.skater.games.map(game => {
-            const {event} = game;
-            const opponent = game.skaters.find(
-              skater => skater.id !== props.skater.id
-            );
-            return (
-              <TableRow key={game.id}>
-                <TableCell>{opponent.full_name}</TableCell>
-                <TableCell>{event.short_name}</TableCell>
-                <TableCell>{game.round_name}</TableCell>
-                <TableCell>{game.win ? 'Win' : 'Loss'}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </StyledTable>
-    </DialogContent>
-  </FormDialogContent>
-);
+class SkaterDialogContent extends Component {
+  static propTypes = {
+    skater: PropTypes.object.isRequired
+  };
 
-SkaterDialogContent.propTypes = {
-  skater: PropTypes.object.isRequired
-};
+  state = {
+    game: null
+  };
+
+  onGameClick = game => this.setState({game});
+
+  onBackClick = () => this.setState({game: null});
+
+  render() {
+    if (this.state.game) {
+      return (
+        <Fragment>
+          <DialogTitle disableTypography>
+            <ButtonBase onClick={this.onBackClick}>
+              <StyledChevronLeftIcon />
+              <Typography variant="title">
+                Back to {this.props.skater.full_name}
+              </Typography>
+            </ButtonBase>
+          </DialogTitle>
+          <DialogContent>
+            {this.state.game.skaters.map(skater => (
+              <DialogContentText key={skater.id}>
+                {skater.full_name}
+              </DialogContentText>
+            ))}
+          </DialogContent>
+        </Fragment>
+      );
+    }
+
+    return (
+      <FormDialogContent data={this.props.skater} formComponent={SkaterForm}>
+        <DialogTitle>{this.props.skater.full_name}</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={theme.spacing.unit * 2}>
+            <GridItem>
+              <DialogContentText>
+                Record: {this.props.skater.wins}-{this.props.skater.losses}
+              </DialogContentText>
+              <DialogContentText>
+                Hometown: {this.props.skater.hometown || UNKNOWN}
+              </DialogContentText>
+            </GridItem>
+            <GridItem>
+              <DialogContentText>
+                Stance: {this.props.skater.stance || UNKNOWN}
+              </DialogContentText>
+              {this.props.skater.birth_date && (
+                <DialogContentText>
+                  Age: {differenceInYears(NOW, this.props.skater.birth_date)}
+                </DialogContentText>
+              )}
+            </GridItem>
+          </Grid>
+          <StyledTable padding="none">
+            <TableHead>
+              <TableRow>
+                <TableCell>Opponent</TableCell>
+                <TableCell>Event</TableCell>
+                <TableCell>Round</TableCell>
+                <TableCell>Result</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.props.skater.games.map(game => {
+                const {event} = game;
+                const opponent = game.skaters.find(
+                  skater => skater.id !== this.props.skater.id
+                );
+                return (
+                  <TableRow
+                    hover
+                    key={game.id}
+                    onClick={() => this.onGameClick(game)}
+                  >
+                    <TableCell>{opponent.full_name}</TableCell>
+                    <TableCell>{event.short_name}</TableCell>
+                    <TableCell>{game.round_name}</TableCell>
+                    <TableCell>{game.win ? 'Win' : 'Loss'}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </StyledTable>
+        </DialogContent>
+      </FormDialogContent>
+    );
+  }
+}
 
 export default SkaterDialogContent;
