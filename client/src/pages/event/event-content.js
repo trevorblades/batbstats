899b6e524ced4d207c98af047c882b2c4a2d8e9f
Trevor Blades <tdblades@gmail.com>
2018-09-12 13:06:58 -0400

    Add games to an event

--- client/src/pages/event/event-content.js
+++ client/src/pages/event/event-content.js
@@ -4,16 +4,19 @@ import PropTypes from 'prop-types';
 import React, {Fragment} from 'react';
 import Typography from '@material-ui/core/Typography';
 
-const EventContent = props => (
-  <Fragment>
-    <Helmet>
-      <title>{props.event.name}</title>
-    </Helmet>
-    <Header>
-      <Typography variant="headline">{props.event.name}</Typography>
-    </Header>
-  </Fragment>
-);
+const EventContent = props => {
+  console.log(props.event);
+  return (
+    <Fragment>
+      <Helmet>
+        <title>{props.event.name}</title>
+      </Helmet>
+      <Header>
+        <Typography variant="headline">{props.event.name}</Typography>
+      </Header>
+    </Fragment>
+  );
+};
 
 EventContent.propTypes = {
   event: PropTypes.object.isRequired
--- client/src/util/event.js
+++ client/src/util/event.js
@@ -1,9 +1,15 @@
+import groupBy from 'lodash/groupBy';
 import map from 'lodash/map';
 import sortBy from 'lodash/sortBy';
 import uniqBy from 'lodash/uniqBy';
 
 export function getEventsFromGames(games) {
-  return sortBy(uniqBy(map(games, 'event'), 'id'), 'id');
+  const events = sortBy(uniqBy(map(games, 'event'), 'id'), 'id');
+  const groups = groupBy(games, 'event.id');
+  return events.map(event => ({
+    ...event,
+    games: groups[event.id]
+  }));
 }
 
 const pattern = /^battle at the berrics ([\d\w]+)(: [\w\\. ]+)?$/i;
