0313c7efab4fc2fd8495035d1c41b0cbdb6d946d
Trevor Blades <tdblades@gmail.com>
2018-09-14 13:00:25 -0400

    Create a bracket object

--- client/src/pages/event/event-content.js
+++ client/src/pages/event/event-content.js
@@ -1,11 +1,42 @@
+import DialogContent from '@material-ui/core/DialogContent';
 import Header from '../../components/header';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import React, {Fragment} from 'react';
 import Typography from '@material-ui/core/Typography';
+import groupBy from 'lodash/groupBy';
+import map from 'lodash/map';
+import reject from 'lodash/reject';
+import styled from 'react-emotion';
+import values from 'lodash/values';
+
+const StyledDialogContent = styled(DialogContent)({
+  overflowY: 'visible'
+});
+
+function addGameChildren(game, rounds, index) {
+  const children = rounds[index];
+  if (!children) {
+    return game;
+  }
+
+  return {
+    ...game,
+    children: children
+      .filter(child => {
+        const skaters = map(child.skaters, 'id');
+        return game.skaters.some(skater => skaters.includes(skater.id));
+      })
+      .map(child => addGameChildren(child, rounds, index + 1))
+  };
+}
 
 const EventContent = props => {
-  console.log(props.event);
+  const rounds = values(
+    groupBy(reject(props.event.games, ['round', 5]), 'round')
+  ).reverse();
+
+  const bracket = addGameChildren(rounds[0][0], rounds, 1);
   return (
     <Fragment>
       <Helmet>
@@ -14,6 +45,7 @@ const EventContent = props => {
       <Header>
         <Typography variant="headline">{props.event.name}</Typography>
       </Header>
+      <StyledDialogContent>{bracket.children.length}</StyledDialogContent>
     </Fragment>
   );
 };
