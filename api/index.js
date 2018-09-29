f5a05f8b5d2dfb6b7f60995594ac2a0bbe0fbeb0
Trevor Blades <tdblades@gmail.com>
2018-09-29 00:31:41 -0700

    Use apollo

--- api/index.js
+++ api/index.js
@@ -1,30 +1,54 @@
-import basicStrategy from './strategies/basic';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import express from 'express';
-import jwtStrategy from './strategies/jwt';
-import passport from 'passport';
-import routes from './routes';
-import {sequelize} from './models';
+import {ApolloServer, gql} from 'apollo-server';
+import {Game} from './models';
 
-const app = express();
-app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(
-  cors({
-    origin: /^https?:\/\/(localhost(:\d{4})?|batbstats\.trevorblades\.com)$/
-  })
-);
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  type Skater {
+    id: ID
+    first_name: String
+    last_name: String
+    full_name: String
+    stance: String
+    birth_date: String
+    country: String
+  }
 
-passport.use(basicStrategy);
-passport.use(jwtStrategy);
+  type Game {
+    id: ID
+    round: Int
+    date: String
+    video_id: String
+    skaters: [Skater]
+  }
 
-app.use('/', routes);
+  type Query {
+    games: [Game]
+  }
+`;
 
-sequelize
-  .sync()
-  .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Listening on port ${process.env.PORT}`)
-    )
-  );
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Game: {
+    skaters: game => game.getSkaters()
+  },
+  Skater: {
+    full_name: skater =>
+      [skater.first_name, skater.last_name].filter(Boolean).join(' ')
+  },
+  Query: {
+    games: () => Game.findAll()
+  }
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({typeDefs, resolvers});
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({url}) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
--- api/models/skater.js
+++ api/models/skater.js
@@ -4,14 +4,6 @@ export default (sequelize, DataTypes) => {
   const Skater = sequelize.define('skater', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    full_name: {
-      type: DataTypes.VIRTUAL(DataTypes.STRING, ['first_name', 'last_name']),
-      get() {
-        return [this.get('first_name'), this.get('last_name')]
-          .filter(Boolean)
-          .join(' ');
-      }
-    },
     stance: DataTypes.ENUM(STANCES),
     birth_date: DataTypes.DATE,
     country: DataTypes.STRING
