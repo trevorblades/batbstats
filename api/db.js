035eb6aff0cc90a30f0074f208289df8e27a844e
Trevor Blades <tdblades@gmail.com>
2021-07-26 21:57:35 -0700

    Sequelize-ify the api

---
+++ api/db.js
@@ -0,0 +1,122 @@
+import {Sequelize} from 'sequelize';
+
+const {DataTypes, Model} = Sequelize;
+
+export const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const STANCE_ENUM = DataTypes.ENUM('regular', 'goofy');
+
+export class Skater extends Model {}
+Skater.init(
+  {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    stance: STANCE_ENUM,
+    birthDate: DataTypes.DATE,
+    country: DataTypes.STRING
+  },
+  {
+    sequelize,
+    modelName: 'skater'
+  }
+);
+
+const VARIATION_ENUM = DataTypes.ENUM('switch', 'nollie', 'fakie');
+
+export class Trick extends Model {}
+Trick.init(
+  {
+    name: DataTypes.STRING,
+    variation: VARIATION_ENUM,
+    spin: DataTypes.INTEGER,
+    flip: DataTypes.INTEGER,
+    shuv: DataTypes.INTEGER,
+    other: DataTypes.BOOLEAN
+  },
+  {
+    sequelize,
+    modelName: 'trick'
+  }
+);
+
+export class Event extends Model {}
+Event.init(
+  {
+    // image: DataTypes.STRING,
+    name: DataTypes.STRING
+  },
+  {
+    sequelize,
+    modelName: 'event'
+  }
+);
+
+export class Game extends Model {}
+Game.init(
+  {
+    round: DataTypes.INTEGER,
+    video: DataTypes.STRING,
+    date: DataTypes.DATE
+  },
+  {
+    sequelize,
+    modelName: 'game'
+  }
+);
+
+Game.belongsTo(Event);
+Event.hasMany(Game);
+
+const MOVE_ENUM = DataTypes.ENUM('rock', 'paper', 'scissors');
+
+class Roshambo extends Model {}
+Roshambo.init(
+  {
+    round: DataTypes.INTEGER,
+    move: MOVE_ENUM
+  },
+  {
+    sequelize,
+    modelName: 'roshambo'
+  }
+);
+
+Roshambo.belongsTo(Game);
+Game.hasMany(Roshambo);
+
+Roshambo.belongsTo(Skater);
+Skater.hasMany(Roshambo);
+
+class Participant extends Model {}
+Participant.init(
+  {},
+  {
+    sequelize,
+    modelName: 'participant'
+  }
+);
+
+Participant.belongsTo(Skater, {as: 'replacement'});
+Skater.hasMany(Participant, {as: 'replacement'});
+
+Game.belongsToMany(Skater, {through: Participant});
+Skater.belongsToMany(Game, {through: Participant});
+
+class Attempt extends Model {}
+Attempt.init(
+  {
+    successful: DataTypes.BOOLEAN,
+    offense: DataTypes.BOOLEAN,
+    redos: DataTypes.INTEGER
+  },
+  {
+    sequelize,
+    modelName: 'attempt'
+  }
+);
+
+Attempt.belongsTo(Game);
+Game.hasMany(Attempt);
+
+Attempt.belongsTo(Trick);
+Trick.hasMany(Attempt);
--- api/index.js
+++ api/index.js
@@ -1,36 +1,115 @@
-const jwt = require('jsonwebtoken');
-const {ApolloServer} = require('apollo-server');
-const {resolvers, typeDefs} = require('./schema');
-const knex = require('knex');
+import {ApolloServer, gql} from 'apollo-server';
+import {DateResolver, DateTypeDefinition} from 'graphql-scalars';
+import {Event, Skater, Trick, sequelize} from './db.js';
+import {makeExecutableSchema} from '@graphql-tools/schema';
 
-const db = knex(process.env.DATABASE_URL);
+const typeDefs = gql`
+  type Query {
+    event(id: ID!): Event
+    events: [Event!]!
+    skater(id: ID!): Skater
+    skaters: [Skater!]!
+    trick(id: ID!): Trick
+    tricks: [Trick!]!
+  }
+
+  type Event {
+    name: String!
+    games: [Game!]!
+  }
+
+  type Game {
+    round: Int!
+    video: String
+    date: Date
+    skaters: [Skater!]!
+    attempts: [Attempt!]!
+    roshambos: [Roshambo!]!
+  }
+
+  type Skater {
+    firstName: String
+    lastName: String
+    stance: Stance
+    birthDate: Date
+    country: String
+    replacement: Skater
+  }
+
+  enum Stance {
+    regular
+    goofy
+  }
+
+  type Trick {
+    name: String!
+    variation: Variation
+    spin: Int!
+    flip: Int!
+    shuv: Int!
+    other: Boolean!
+  }
+
+  enum Variation {
+    switch
+    nollie
+    fakie
+  }
+
+  type Attempt {
+    successful: Boolean!
+    offense: Boolean!
+    redos: Int!
+    trick: Trick!
+    skater: Skater!
+  }
+
+  type Roshambo {
+    round: Int!
+    move: Move!
+    skater: Skater!
+  }
+
+  enum Move {
+    rock
+    paper
+    scissors
+  }
+`;
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  introspection: true,
-  playground: true,
-  async context({req}) {
-    const context = {db};
-
-    if (req.headers.authorization) {
-      const matches = req.headers.authorization.match(/bearer (.+)/i);
-      if (matches) {
-        try {
-          const {sub} = jwt.verify(matches[1], process.env.JWT_SECRET);
-          context.user = await db('users')
-            .where('id', sub)
-            .first();
-        } catch (error) {
-          // let errors pass
-        }
+  schema: makeExecutableSchema({
+    typeDefs: [DateTypeDefinition, typeDefs],
+    resolvers: {
+      Date: DateResolver,
+      Query: {
+        event: (_, {id}) => Event.findByPk(id),
+        events: () => Event.findAll(),
+        skater: (_, {id}) => Skater.findByPk(id),
+        skaters: () => Skater.findAll(),
+        trick: (_, {id}) => Trick.findByPk(id),
+        tricks: () => Trick.findAll()
+      },
+      Event: {
+        games: event => event.getGames()
+      },
+      Game: {
+        skaters: game => game.getSkaters(),
+        attempts: game => game.getAttempts(),
+        roshambos: game => game.getRoshambos()
+      },
+      Skater: {
+        replacement: skater => skater.participant?.getReplacement()
+      },
+      Attempt: {
+        trick: attempt => attempt.getTrick(),
+        skater: attempt => attempt.getSkater()
       }
     }
-
-    return context;
-  }
+  })
 });
 
-server.listen({port: process.env.PORT}).then(({url}) => {
+sequelize.sync().then(async () => {
+  const {url} = await server.listen({port: process.env.PORT});
   console.log(`🚀 Server ready at ${url}`);
 });
