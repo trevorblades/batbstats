d726b196345ec780c9ba2fe6e128aa5422af0d48
Trevor Blades <tdblades@gmail.com>
2021-11-23 23:01:18 -0800

    Lockout mutations

--- api/index.js
+++ api/index.js
@@ -1,15 +1,23 @@
 import {ApolloServer} from 'apollo-server';
 import {DateTimeTypeDefinition, DateTypeDefinition} from 'graphql-scalars';
+import {applyMiddleware} from 'graphql-middleware';
 import {createContext} from 'dataloader-sequelize';
+import {deny, shield} from 'graphql-shield';
 import {makeExecutableSchema} from '@graphql-tools/schema';
 import {resolvers, typeDefs} from './schema.js';
 import {sequelize} from './db.js';
 
+const schema = makeExecutableSchema({
+  typeDefs: [DateTypeDefinition, DateTimeTypeDefinition, typeDefs],
+  resolvers
+});
+
+const permissions = shield({
+  Mutation: deny
+});
+
 const server = new ApolloServer({
-  schema: makeExecutableSchema({
-    typeDefs: [DateTypeDefinition, DateTimeTypeDefinition, typeDefs],
-    resolvers
-  }),
+  schema: applyMiddleware(schema, permissions),
   context() {
     return {context: createContext(sequelize)};
   }
